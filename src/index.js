ebefd757986a7bf08459932bff01fb6a7ea1df93
kanchi <KanchiVaghela.ict23@adaniuni.ac.in>
2026-05-24 16:14:24 +0530

    fix: Express 5 wildcard syntax for production static serving

--- src/index.js
+++ src/index.js
@@ -30,9 +30,9 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-  });
+  app.get('*splat', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 }
 
 const PORT = process.env.PORT || 3000;
